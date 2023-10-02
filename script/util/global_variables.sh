f0a673f9cad7ca140d8f0f37db228039141704e7
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2023-10-02 12:09:13 -0700

    updated global variable declarations

--- script/util/global_variables.sh
+++ script/util/global_variables.sh
@@ -1,12 +1,11 @@
-current_dir=$(realpath "$(dirname "$(dirname "${BASH_SOURCE[0]}")")")
-ROOT_PATH="$(dirname "${current_dir}")"
+current_dir=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
+ROOT_DIR="$(dirname "${current_dir}")"
 
-# Sdkman Java versions
+# sdkman Java versions
 JAVA8="8.0.382-amzn"
 JAVA17="17.0.8-oracle"
 
 # directories
-ROOT_DIR=$(dirname "$(dirname "$(realpath "$(dirname "${BASH_SOURCE[0]}")")")")
 RESOURCES_DIR="${ROOT_DIR}/script/resources"
 UTIL_DIR="${ROOT_DIR}/script/util"
 DEFECTS4J_DIR="${ROOT_DIR}/defects4j"
@@ -16,7 +15,7 @@ SDKMAN_DIR="${RESOURCES_DIR}/sdkman"
 SERVER_PORT=5050
 
 # export variables
-export ROOT_PATH
+export ROOT_DIR
 export JAVA8
 export JAVA17
 export UTIL_DIR
