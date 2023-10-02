4de96946812df55420625544e2e60a5c9cebc4e7
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2023-10-02 12:19:29 -0700

    added EvoSuite setup in init.sh

--- script/init.sh
+++ script/init.sh
@@ -8,7 +8,12 @@ cd "${ROOT_DIR}" || exit 1
 bash "${UTIL_DIR}/install_sdkman.sh"
 source "${UTIL_DIR}/init_sdkman.sh"
 # download EvoSuite
-wget https://github.com/EvoSuite/evosuite/releases/download/v1.0.6/evosuite-1.0.6.jar
+if [ ! -f "${ROOT_DIR}/evosuite-1.0.6.jar" ]; then
+  wget https://github.com/EvoSuite/evosuite/releases/download/v1.0.6/evosuite-1.0.6.jar
+  echo "Successfully installed EvoSuite."
+else
+  echo "EvoSuite is already installed."
+fi
 
 # install Java 8
 sdk install java 8.0.382-amzn
