823f2edce4808042b11e4f22057bfeb0b1663931
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2023-10-04 09:27:55 -0700

    added global variables

--- script/util/global_variables.sh
+++ script/util/global_variables.sh
@@ -6,9 +6,10 @@ JAVA8="8.0.382-amzn"
 JAVA17="17.0.8-oracle"
 
 # directories
-RESOURCES_DIR="${ROOT_DIR}/script/resources"
-UTIL_DIR="${ROOT_DIR}/script/util"
 DEFECTS4J_DIR="${ROOT_DIR}/defects4j"
+SCRIPT_DIR="${ROOT_DIR}/script"
+UTIL_DIR="${SCRIPT_DIR}/util"
+RESOURCES_DIR="${SCRIPT_DIR}/resources"
 SDKMAN_DIR="${RESOURCES_DIR}/sdkman"
 
 # server
@@ -18,7 +19,9 @@ SERVER_PORT=5050
 export ROOT_DIR
 export JAVA8
 export JAVA17
-export UTIL_DIR
 export DEFECTS4J_DIR
+export SCRIPT_DIR
+export UTIL_DIR
+export RESOURCES_DIR
 export SDKMAN_DIR
 export SERVER_PORT
