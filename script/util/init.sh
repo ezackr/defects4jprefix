522cb52e17870e80b4bfeaeb6b45599bd4ed9092
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2023-10-05 13:44:09 -0700

    fixed path in init.sh

--- script/util/init.sh
+++ script/util/init.sh
@@ -1,7 +1,7 @@
 # get current directory
 current_dir=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
 # setup global variables
-. "${current_dir}/util/global_variables.sh"
+. "${current_dir}/global_variables.sh"
 cd "${ROOT_DIR}" || exit 1
 
 # download EvoSuite
