157c2683b286c82b26e601398fb7c84ad963f994
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2023-10-05 18:04:09 -0700

    added secondary call to output.sh to correctly log results

--- script/prefix.sh
+++ script/prefix.sh
@@ -19,5 +19,7 @@ while IFS=, read -r project_id bug_id modified_class; do
     echo -e "Unable to find system binaries for project ${project_id}."
     exit 1
   fi
+  # generate prefixes using evosuite
   bash "${current_dir}/evosuite.sh" "${modified_class}" "${project_dir}/${binary_path}"
+  bash "${current_dir}/util/output.sh" "${project_id}" "${bug_id}" "${modified_class}"
 done < "${root_dir}/modified_classes.csv"
