41bf42e293f90a374444b88623caf65431ed2144
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2023-12-12 16:42:11 -0800

    removed automated EvoSuite installation

--- script/util/init.sh
+++ script/util/init.sh
@@ -3,15 +3,6 @@ current_dir=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
 # setup global variables
 . "${current_dir}/global_variables.sh"
 cd "${ROOT_DIR}" || exit 1
-
-# download EvoSuite
-if [ ! -f "${ROOT_DIR}/evosuite-1.0.6.jar" ]; then
-  wget https://github.com/EvoSuite/evosuite/releases/download/v1.0.6/evosuite-1.0.6.jar
-  echo "Successfully installed EvoSuite."
-else
-  echo "EvoSuite is already installed."
-fi
-chmod +x "${ROOT_DIR}/evosuite-1.0.6.jar"
 # download sdkman
 bash "${UTIL_DIR}/install_sdkman.sh"
 source "${UTIL_DIR}/init_sdkman.sh"
