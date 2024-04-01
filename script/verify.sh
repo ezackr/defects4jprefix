df0bf5611b3f095584f21988de75ed501cc3cd3e
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2024-04-01 13:08:26 -0700

    optimized verification script

--- script/verify.sh
+++ script/verify.sh
@@ -10,6 +10,7 @@ source "${current_dir}/util/init_sdkman.sh"
 if [ ! -d "${root_dir}/test-suite" ]; then
   mkdir "${root_dir}/test-suite"
 fi
+sdk use java "8.0.382-amzn"
 while IFS=, read -r project_id bug_id _; do
   # check for a given project or bug id
   if [ ${#} -gt 0 ]; then
@@ -22,9 +23,12 @@ while IFS=, read -r project_id bug_id _; do
       fi
     fi
   fi
-  sdk use java "8.0.382-amzn"
+  if [ ! -d "${root_dir}/src/main/evosuite-prefixes/${project_id}/${bug_id}" ]; then
+    echo -e "Unable to find prefixes for project ${project_id} bug ${bug_id}"
+    continue
+  fi
   # compress test suite
-  cd "${root_dir}/src/main/evosuite-tests/${project_id}/${bug_id}" || exit 1
+  cd "${root_dir}/src/main/evosuite-prefixes/${project_id}/${bug_id}" || exit 1
   tar -cvjSf "${project_id}-${bug_id}b-evosuite.tar.bz2" .
   mv "${project_id}-${bug_id}b-evosuite.tar.bz2" "${root_dir}/test-suite"
   cd - || exit 1
