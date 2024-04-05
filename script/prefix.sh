1dca55f3871d84389ad7f6f8a302790b26c8f1e7
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2024-04-05 08:51:49 -0700

    fixed issues with variable names in prefix.sh

--- script/prefix.sh
+++ script/prefix.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 # This script generates test prefixes for all bugs in Defects4J.
 current_dir=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
-# Setup global variables
-source "${current_dir}/util/global_variables.sh"
+root_dir=$(dirname "${current_dir}")
 # setup defects4j and sdkman
 export PATH=$PATH:"${DEFECTS4J_HOME}"/framework/bin
 source "${current_dir}/util/init_sdkman.sh"
@@ -21,41 +20,41 @@ while IFS=, read -r project_id bug_id modified_class; do
   fi
   # check if prefixes are already generated
   qualifiers=$(echo "${modified_class}" | sed 's/\./\//g')
-  if [ -f "${ROOT_DIR}/src/main/evosuite-prefixes/${project_id}/${bug_id}/${qualifiers}_ESTest.java" ]; then
+  if [ -f "${root_dir}/src/main/evosuite-prefixes/${project_id}/${bug_id}/${qualifiers}_ESTest.java" ]; then
     echo "Test prefixes already generated for ${project_id}-${bug_id}."
     continue
   fi
-  if [ ! -f "${ROOT_DIR}/src/main/evosuite-tests/${project_id}/${bug_id}/${qualifiers}_ESTest.java" ]; then
+  if [ ! -f "${root_dir}/src/main/evosuite-tests/${project_id}/${bug_id}/${qualifiers}_ESTest.java" ]; then
     # generate new prefixes
-    sdk use java "$JAVA8"
-    echo "${modified_class}" > "${ROOT_DIR}/modified_class.txt"
-    mkdir "${ROOT_DIR}/output"
+    sdk use java "8.0.382-amzn"
+    echo "${modified_class}" > "${root_dir}/modified_class.txt"
+    mkdir "${root_dir}/output"
     gen_tests.pl -g "evosuite" \
       -p "${project_id}" \
       -v "${bug_id}b" \
       -n 0 \
-      -o "${ROOT_DIR}/output" \
+      -o "${root_dir}/output" \
       -b 300 \
-      -c "${ROOT_DIR}/modified_class.txt" \
+      -c "${root_dir}/modified_class.txt" \
       -s 13042023 \
-      -t "${ROOT_DIR}/temp"
+      -t "${root_dir}/temp"
     # decompress generated tests
-    cd "${ROOT_DIR}/output/${project_id}/evosuite/0" || { echo -e "Unable to generate tests for ${project_id}-${bug_id}."; continue; }
+    cd "${root_dir}/output/${project_id}/evosuite/0" || { echo -e "Unable to generate tests for ${project_id}-${bug_id}."; continue; }
     tar -xf "${project_id}-${bug_id}b-evosuite.0.tar.bz2"
     rm "${project_id}-${bug_id}b-evosuite.0.tar.bz2"
     cd - || exit 1
     # move tests to output directory
-    mv "${ROOT_DIR}/output/${project_id}/evosuite/0" "${ROOT_DIR}/output/evosuite-tests"
+    mv "${root_dir}/output/${project_id}/evosuite/0" "${root_dir}/output/evosuite-tests"
   else
     # move tests from main to output directory
     echo "EvoSuite tests already generated for ${project_id}-${bug_id}."
-    mkdir -p "${ROOT_DIR}/output/evosuite-tests/$(dirname "${qualifiers}")"
-    mv "${ROOT_DIR}/src/main/evosuite-tests/${project_id}/${bug_id}/${qualifiers}_ESTest.java" "${ROOT_DIR}/output/evosuite-tests/${qualifiers}_ESTest.java"
-    mv "${ROOT_DIR}/src/main/evosuite-tests/${project_id}/${bug_id}/${qualifiers}_ESTest_scaffolding.java" "${ROOT_DIR}/output/evosuite-tests/${qualifiers}_ESTest_scaffolding.java"
+    mkdir -p "${root_dir}/output/evosuite-tests/$(dirname "${qualifiers}")"
+    mv "${root_dir}/src/main/evosuite-tests/${project_id}/${bug_id}/${qualifiers}_ESTest.java" "${root_dir}/output/evosuite-tests/${qualifiers}_ESTest.java"
+    mv "${root_dir}/src/main/evosuite-tests/${project_id}/${bug_id}/${qualifiers}_ESTest_scaffolding.java" "${root_dir}/output/evosuite-tests/${qualifiers}_ESTest_scaffolding.java"
   fi
   # remove oracles
-  sdk use java "$JAVA17"
-  java -jar "${ROOT_DIR}/remover.jar" "remove_oracles" "${ROOT_DIR}/output/evosuite-tests"
+  sdk use java "17.0.8-oracle"
+  java -jar "${root_dir}/remover.jar" "remove_oracles" "${root_dir}/output/evosuite-tests"
   # move and cleanup output
   bash "${current_dir}/util/output.sh" "${project_id}" "${bug_id}" "${modified_class}"
-done < "${ROOT_DIR}/modified_classes.csv"
+done < "${root_dir}/modified_classes.csv"
